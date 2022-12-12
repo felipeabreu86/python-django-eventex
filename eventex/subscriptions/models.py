4ca03bdfe28aed9016f4481e64aa57ad3786b5b9
FELIPE MARTINS ABREU <felipe.abreu@trt1.jus.br>
2022-12-12 15:10:53 -0300

    Adiciona campo paid

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -3,10 +3,11 @@ from django.db import models
 
 class Subscription(models.Model):
     name = models.CharField('nome', max_length=100)
-    cpf = models.CharField('CPF', max_length=1)
+    cpf = models.CharField('CPF', max_length=11)
     email = models.EmailField('e-mail')
     phone = models.CharField('telefone', max_length=20)
     created_at = models.DateTimeField('criado em', auto_now_add=True)
+    paid = models.BooleanField('pago', default=False)
 
     class Meta:
         verbose_name_plural = 'inscrições'
